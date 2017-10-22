2b549def98a06c98fc1bc75ef9f212aa66f03328
Brandon Myers <bmyers1788@gmail.com>
2017-10-22 15:34:41 -0500

    fix buggy starter code
    * ints were confused with byte addresses
    * words were confused with bytes
    * I made it harder to become confused when you read/write a bad AXI address

--- dma/dma_test.c
+++ dma/dma_test.c
@@ -17,27 +17,42 @@
 //See http://forums.xilinx.com/t5/Zynq-All-Programmable-SoC/Bram-parameters-in-Xparameters-h/m-p/555182/highlight/true#M5042
 #undef XPAR_AXI_BRAM_CTRL_0_BASEADDR
 #undef XPAR_AXI_BRAM_CTRL_0_HIGHADDR
-#define XPAR_AXI_BRAM_CTRL_0_BASEADDR 0x40000000
+#define XPAR_AXI_BRAM_CTRL_0_BASEADDR 0x40000000  // BM: based on address editor in Vivado
 #define XPAR_AXI_BRAM_CTRL_0_HIGHADDR 0x40001FFF
 
 #define BRAM_MEMORY XPAR_AXI_BRAM_CTRL_0_BASEADDR
+#define BRAM_MEMORY_LAST_WORD (XPAR_AXI_BRAM_CTRL_0_HIGHADDR-3)
 
 #define RESET_LOOP_COUNT	10	// Number of times to check reset is done
-#define LENGTH 8192 // source and destination buffers lengths in number of words
+#define LENGTH 4096 // source and destination buffers lengths in number of words
+
+// BM: I'm assuming this means you want to avoid writing over our  program
+// since in the standalone/baremetal mode there is no memory protection
 #define DDR_MEMORY XPAR_PS7_DDR_0_S_AXI_BASEADDR+0x00020000 // pass all code and data sections
+
+// BM: values used by the timer initialization
 #define TIMER_DEVICE_ID	XPAR_SCUTIMER_DEVICE_ID
+// BM: just seems like this is the initial value of the timer
+// but why is it important to be -1 / maxuint?
 #define TIMER_LOAD_VALUE 0xFFFFFFFF
+// BM: not sure what these are, but I think interrupt related?
 #define DMA0_ID XPAR_XDMAPS_1_DEVICE_ID
 #define INTC_DEVICE_INT_ID XPAR_SCUGIC_SINGLE_DEVICE_ID
 
+// BM: these are two variables used by the interrupt handlers
 volatile static int Done = 0;	/* Dma transfer is done */
 volatile static int Error = 0;	/* Dma Bus Error occurs */
 
+// BM: used for stdin in this app
 XUartPs Uart_PS;		/* Instance of the UART Device */
+// BM: low level device for getting clock ticks (just for timing the transfers)
 XScuTimer Timer;		/* Cortex A9 SCU Private Timer Instance */
 XDmaPs Dma;				/* PS DMA */
 XScuGic Gic;			/* PS GIC */
 
+
+// BM: I guess they decide not to use scanf
+//     however this example is useful! since maybe I can reuse the code for other stuff
 int getNumber (){
 
 	u8 byte;
@@ -100,6 +115,7 @@ int getNumber (){
 	}
 }
 
+// BM: interrupt handlers for DMA transfer
 void DmaDoneHandler(unsigned int Channel,
 		    XDmaPs_Cmd *DmaCmd,
 		    void *CallbackRef)
@@ -133,7 +149,7 @@ int SetupIntrSystem(XScuGic *GicPtr, XDmaPs *DmaPtr)
 	// for the device occurs, the device driver handler performs the specific
 	// interrupt processing for the device
 
-	// Connect the Fault ISR
+	// Connect the Fault ISR (BM: Interrupt service routine)
 	Status = XScuGic_Connect(GicPtr,
 				 XPAR_XDMAPS_0_FAULT_INTR,
 				 (Xil_InterruptHandler)XDmaPs_FaultISR,
@@ -156,6 +172,7 @@ int SetupIntrSystem(XScuGic *GicPtr, XDmaPs *DmaPtr)
 	return XST_SUCCESS;
 }
 
+// BM: get the choice of transfer
 u8 menu(void)
 {
 	u8 byte;
@@ -164,23 +181,40 @@ u8 menu(void)
 	print("Enter 2 for BRAM to DDR3 transfer\r\n");
 	print("Enter 3 for DDR3 to DDR3 transfer\r\n");
 	print("Enter 4 to exit\r\n");
+
 	while (!XUartPs_IsReceiveData(STDIN_BASEADDRESS));
 	byte = XUartPs_ReadReg(STDIN_BASEADDRESS,
 						    XUARTPS_FIFO_OFFSET);
+
+	// BM: I changed this so that I could throw out what I think
+		// is a newline can get thrown out
+		while (!XUartPs_IsReceiveData(STDIN_BASEADDRESS));
+		/*ignore return value*/XUartPs_ReadReg(STDIN_BASEADDRESS,
+								    XUARTPS_FIFO_OFFSET);
+
 	return(byte);
 
 }
 
-u8 byte_sel(void)
+// BM: get the number of words to transfer
+u8 word_sel(void)
 {
-	u8 byte;
+	u8 choice;
 
    	print("Enter number of words you want to transfer:\r\n");
    	print("1=256; 2=512; 3=1024; 4=2048; 5=4096; 6=8192;\r\n");
+
  	while (!XUartPs_IsReceiveData(STDIN_BASEADDRESS));
-	byte = XUartPs_ReadReg(STDIN_BASEADDRESS,
+	choice = XUartPs_ReadReg(STDIN_BASEADDRESS,
 						    XUARTPS_FIFO_OFFSET);
-	return(byte);
+
+	// BM: I changed this so that I could throw out what I think
+	   		// is a newline can get thrown out
+	   		while (!XUartPs_IsReceiveData(STDIN_BASEADDRESS));
+	   		/*ignore return value*/XUartPs_ReadReg(STDIN_BASEADDRESS,
+	   								    XUARTPS_FIFO_OFFSET);
+
+	return(choice);
 }
 
 int main (void) {
@@ -206,8 +240,9 @@ int main (void) {
 
 	// PS DMA related definitions
 	XDmaPs_Config *DmaCfg;
-	XDmaPs_Cmd DmaCmd = {
-		.ChanCtrl = {
+	// BM: What is so special about these particular choices?
+	XDmaPs_Cmd DmaCmd = { // BM: no custom dma program...
+		.ChanCtrl = {  // BM: XDmaPs_ChanCtrl
 			.SrcBurstSize = 4,
 			.SrcBurstLen = 4,
 			.SrcInc = 1,		// increment source address
@@ -296,56 +331,68 @@ int main (void) {
 
     test_done = 0;
 
+   	print("start the test\r\n");
     while(test_done==0)
     {
 
-   	   	num_in = byte_sel();
+   	   	num_in = word_sel();
 
    	   	switch(num_in)
     			{
     				case '1' :
     					num = 256;
-    					print("Sending 256 bytes\r\n");
+    					print("Sending 256 words\r\n");
     					break;
     				case '2' :
     					num = 512;
-    					print("Sending 512 bytes\r\n");
+    					print("Sending 512 words\r\n");
     					break;
     				case '3' :
     					num = 1024;
-    					print("Sending 1024 bytes\r\n");
+    					print("Sending 1024 words\r\n");
     					break;
     				case '4' :
     					num = 2048;
-    					print("Sending 2048 bytes\r\n");
+    					print("Sending 2048 words\r\n");
     				    break;
     				case '5' :
     					num = 4096;
-    					print("Sending 4096 bytes\r\n");
+    					print("Sending 4096 words\r\n");
     					break;
     				case '6' :
     					num = 8192;
-    					print("Sending 8192 bytes\r\n");
+    					print("Sending 8192 words\r\n");
     					break;
     				default :
     					num = 256;
-    					print("Sending 256 bytes\r\n");
+    					print("Sending 256 words\r\n");
     					break;
     			}
    	   	print("\r\n");
+
    	    select = menu();
 
+   	    // BM: this is interesting that the only difference
+   	    // between the transfer with different types is the memory addresses
     	switch(select)
 		{
 			case '1' :
 				source = (u32 *)BRAM_MEMORY;
 				destination = (u32 *)(BRAM_MEMORY+LENGTH);
 				print("BRAM to BRAM transfer\r\n");
+				if ((int)(destination+num-1) > BRAM_MEMORY_LAST_WORD) {
+						  print("too big of transfer for BRAM destination\r\n");
+						  continue;
+				}
 				break;
 			case '2' :
 				source = (u32 *)BRAM_MEMORY;
 				destination = (u32 *)DDR_MEMORY;
 				print("BRAM to DDR3 transfer\r\n");
+				if ((int)(source+num-1) > BRAM_MEMORY_LAST_WORD) {
+										  print("too big of transfer for BRAM source\r\n");
+										  continue;
+								}
 				break;
 			case '3' :
 				source = (u32 *)DDR_MEMORY;
@@ -361,21 +408,24 @@ int main (void) {
 				break;
 		}
 
-
-
 		if(test_done)
 			break;
 
+		// BM: I'm assuming we are making the experiment fair
+		// want it to be actually from memory not cache
 
-
+		// BM: Awesome! you can flush the cache
 		// Following code is required only if Cache is enabled
 		Xil_DCacheFlushRange((u32)&source, LENGTH);
 		Xil_DCacheFlushRange((u32)&destination, LENGTH);
 
 		// Initialize src memory
-		for (i=0; i<num; i++)
+		print("initialize source memory\r\n");
+		for (i=0; i<num; i++) {
+			//xil_printf("clearing %x of %x\r\n", (int)(source+i), num);
 			*(source+i) = num-i;
-
+		}
+		print("initialized\r\n");
 
 	// Non-DMA mode
 		// reset timer
@@ -383,8 +433,13 @@ int main (void) {
 
 		// start moving data through the processor - no CDMA, no interrupt
 		// gives base consumed cycles
-		for (i=0; i<num; i++)
+
+		print("transferring\r\n");
+		for (i=0; i<num; i++) {
+			//xil_printf("transferring %d\r\n",i);
 			*(destination+i) = *(source+i);
+		}
+		print("transferred\r\n");
 
 		CntValue1 = XScuTimer_GetCounterValue(TimerInstancePtr);
 
@@ -416,6 +471,7 @@ int main (void) {
 		// reset timer
 		XScuTimer_RestartTimer(TimerInstancePtr);
 
+		// BM: wait for DMA to finish
 		while ((Done==0) & (Error==0));
 		if (Error)
 			print("Error occurred during DMA transfer\r\n");
@@ -429,6 +485,7 @@ int main (void) {
 		// Disable the interrupt for the device
 		XScuGic_Disable(&Gic, XPAR_XDMAPS_0_DONE_INTR_0);
 
+		// BM: double check that the transfer *actually* worked
 		for (i = 0; i < num; i++) {
 				if ( destination[i] != source[i]) {
 					xil_printf("Data match failed at = %d, source data = %d, destination data = %d\n\r",i,source[i],destination[i]);
